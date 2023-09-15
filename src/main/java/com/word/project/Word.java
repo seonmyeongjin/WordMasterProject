0db7284622f4a0ef3935882e27568e61f6ab039f
myeongjin <smj22200376@handong.ac.kr>
2023-09-16 02:34:15 +0900

    Initial Commit

--- src/main/java/com/word/project/Word.java
+++ src/main/java/com/word/project/Word.java
@@ -50,6 +50,6 @@ public class Word {
     }
 
     public String toFileString(){
-        return this.level + "|" + this.word +"|" +this.meaning;
+        return this.level + "|" + this.word + "|" +this.meaning;
     }
 }
